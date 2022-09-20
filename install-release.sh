3a1c65121c863117651c2a4b8ab7bfddd003e5a5
PR Aiman <58582776+praiman99@users.noreply.github.com>
2022-09-20 14:32:24 +0800

    update install-release.sh

--- install-release.sh
+++ install-release.sh
@@ -285,7 +285,7 @@ get_version() {
 }
 
 download_v2ray() {
-  DOWNLOAD_LINK="https://github.com/v2fly/v2ray-core/releases/download/v4.45.0/v2ray-linux-64.zip"
+  DOWNLOAD_LINK="https://github.com/v2fly/v2ray-core/releases/download/v4.45.2/v2ray-linux-64.zip"
   echo "Downloading V2Ray archive: $DOWNLOAD_LINK"
   if ! curl -x "${PROXY}" -R -H 'Cache-Control: no-cache' -o "$ZIP_FILE" "$DOWNLOAD_LINK"; then
     echo 'error: Download failed! Please check your network or try again.'
